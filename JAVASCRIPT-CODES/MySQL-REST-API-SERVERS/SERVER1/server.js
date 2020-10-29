fe39d3b82266f55d0d5aaa8c7630ed2210a459d9
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-10-29 21:05:32 +0100

    Add files via upload

---
+++ JAVASCRIPT-CODES/MySQL-REST-API-SERVERS/SERVER1/server.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+
+//HTTPS
+const privateKey  = fs.readFileSync('./ssl/json.mysqhost.tk-le.key', 'utf8');
+const certificate = fs.readFileSync('./ssl/json.mysqhost.tk-le.crt', 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
+
+require("./app/routes/customer.routes.js")(app);
+
+// set port, listen for requests
+
+let httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(3003);
+console.log("SERVER IS RUNNING NOW!")
