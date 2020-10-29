1410c9d13db921500953d51e068649fc8d508e53
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-10-29 21:13:05 +0100

    Add files via upload

---
+++ JAVASCRIPT-CODES/MySQL-REST-API-SERVERS/SERVER2/app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "mysqhost.tk",
+  USER: "[IDE ÍRD AZ ADATBÁZIS FELHASZNÁLÓ NEVET]",
+  PASSWORD: "[IDE ÍRD AZ ADATBÁZIS JELSZÓT]",
+  DB: "[IDE ÍRD AZ ADATBÁZIS NEVET]"
+};
---
+++ JAVASCRIPT-CODES/MySQL-REST-API-SERVERS/SERVER2/app/models/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+const dbConfig = require("../config/db.config.js");
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
+});
+
+// open the MySQL connection
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
+
+module.exports = connection;
+
+setInterval(function () {
+    connection.query('SELECT 1');
+	console.log("KEEEP CONNECTION");
+}, 20000);
